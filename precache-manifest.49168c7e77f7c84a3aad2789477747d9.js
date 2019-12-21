6023291afd874fe23c1fe11e2a0547f07bb701d4
Stefan Wille <github@stefanwille.com>
2019-12-21 21:13:41 +0100

    Updates

---
+++ precache-manifest.49168c7e77f7c84a3aad2789477747d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc4c1cd44f88230895c528a0ed792b4",
+    "url": "/pacman-react/index.html"
+  },
+  {
+    "revision": "35a039b20568b5f41580",
+    "url": "/pacman-react/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e289f671b9cdd70e0263",
+    "url": "/pacman-react/static/css/main.02e9af08.chunk.css"
+  },
+  {
+    "revision": "35a039b20568b5f41580",
+    "url": "/pacman-react/static/js/2.413dcbe1.chunk.js"
+  },
+  {
+    "revision": "7a95538d6331d1ea01f01037df475a30",
+    "url": "/pacman-react/static/js/2.413dcbe1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e289f671b9cdd70e0263",
+    "url": "/pacman-react/static/js/main.e3ff398d.chunk.js"
+  },
+  {
+    "revision": "486ade5482313771bf5a",
+    "url": "/pacman-react/static/js/runtime-main.b069bfd6.js"
+  }
+]);
